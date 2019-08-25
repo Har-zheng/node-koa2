69be7533df97ee2824c1ed24ec938ed84ceb6d35
unknown <1456421562@qq.com>
2019-08-26 00:11:04 +0800

    修复问题

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl/normal')
+const { normal } = require('./tpl/index')
 
 app.use(async (ctx, next)=> {
   ctx.type = 'text/html; charset = utf-8'
--- server/tpl/normal.js
+++ server/tpl/normal.js
@@ -3,14 +3,14 @@ module.exports = `
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, inital-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>koa 学习</title>
-    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css"></link>
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div class =" container" >
+    <div class ="container" >
       <div class="row">
         <div class="col-md-8">
         <h1>Hi ZHZ</h1>
@@ -20,7 +20,7 @@ module.exports = `
         <h1>Hi ZHZ</h1>
         <p> jingtai </p>
         </div>
-      </div>
+        </div>
      </div>
   </body>
 </html>
