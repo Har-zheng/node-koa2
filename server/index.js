e00054fd898c18c2f18cafc198aaf71f329756bf
unknown <1456421562@qq.com>
2019-08-26 13:10:13 +0800

    pug 模板

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl,ejsTpl,pugTpl } = require('./tpl/index')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next)=> {
-  ctx.type = 'text/html; charset = utf-8'
-  ctx.body = pug.render(pugTpl, {
-    you: 'lanlan',
-    me: 'zhz',
+  await ctx.render('index', {
+    you: 'ZHZ',
+    me: 'HongZhen'
   })
-  // ctx.body = htmlTpl
-  // await next()
 })
 const port = process.env.PORT || 8000
 
