c1784a1b0d5761386d0d25d7954647ae96da7427
unknown <1456421562@qq.com>
2019-08-28 00:20:07 +0800

    test

--- server/database/init.js
+++ server/database/init.js
@@ -2,8 +2,8 @@ const mongoose = require('mongooes')
 const db = 'mongodb://localhost/douban-trailer'
 
 mongoose.Promise = global.Promise
-
 exports.connect = () => {
+  let
   if(process.env.NODE_ENV !== 'production'){
     mongoose.set('debug', true)
   }
