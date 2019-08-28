83a2d54e5b5bd292e538b7b275c3ef207a8c178a
unknown <1456421562@qq.com>
2019-08-28 15:47:58 +0800

    feat: mongodb 数据库连接

--- server/database/init.js
+++ server/database/init.js
@@ -1,12 +1,41 @@
-const mongoose = require('mongooes')
-const db = 'mongodb://localhost/douban-trailer'
+const mongoose = require('mongoose')
+const db = 'mongodb://localhost/douban-test'
 
 mongoose.Promise = global.Promise
 exports.connect = () => {
-  let
-  if(process.env.NODE_ENV !== 'production'){
-    mongoose.set('debug', true)
-  }
-
-  mongoose.connect(db)
+  let maxConnectTimes = 0
+  return new Promise((resolve, reject) =>{
+    if(process.env.NODE_ENV !== 'production'){
+      mongoose.set('debug', true)
+    }
+    mongoose.connect(db, { useNewUrlParser: true })
+    mongoose.connection.on('disconnected', () => {
+      maxConnectTimes++
+      if(maxConnectTimes < 5){
+        mongoose.connect(db, { useNewUrlParser: true })
+        resolve()
+      }else{
+        throw new Error('数据库连接失败!!')
+      }
+    })
+    mongoose.connection.on('error',err => {
+      maxConnectTimes++
+      if(maxConnectTimes < 5){
+        mongoose.connect(db, { useNewUrlParser: true })
+        resolve()
+      }else{
+        reject(err)
+        throw new Error('数据库连接失败!!')
+      }
+    })
+    mongoose.connection.once('open', () => {
+      const Dog = mongoose.model('Dog', {name: String})
+      const doga = new Dog({name: '阿尔法'})
+      doga.save().then(() => {
+        console.log('ok!')
+      })
+      resolve()
+      console.log('MongooDB Connected successfuly!')
+    })
+  })
 }
