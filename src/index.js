4235fced373e9ca113ae3e11c671dcde348d67b6
unknown <1456421562@qq.com>
2019-09-03 22:55:07 +0800

    feat: 新增react 结构渲染

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter} from 'react-router-dom'
 import App from '/app'  //路由的控制由app.js文件来完成
 
 const rootElement = document.getElementById('app')
-
+import 'babel-polyfill'
 
 render(
   <BrowserRouter>
---
+++ src/layouts/default.js
@@ -0,0 +1,99 @@
+    
+// 布局文件
+import React, { Component } from 'react'
+import {
+  Menu,
+  Spin
+} from 'antd'
+import navRoutes from '../nav'
+
+// 载入主菜单选择项的对应内容
+const getMenuContent = ({ path = '/', name }) => (
+  <a href={path} style={{color: '#fff2e8'}}>
+    {name}
+  </a>
+)
+
+export default class LayoutDefault extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: false,
+      tip: '再等一下下嘛~'
+    }
+  }
+
+  componentDidMount () {
+    window.__LOADING__ = this.toggleLoading
+    console.log('123布局文件的props:', this.props)
+    console.log('123布局文件的state:', this.state)
+  }
+
+  componentWillUnMount () {
+    window.__LOADING__ = null
+    console.log('456布局文件的props:', this.props)
+    console.log('456布局文件的state:', this.state)
+  }
+
+  matchRouteName = this.props.match
+    ? navRoutes.find(e => e.name === this.props.match.params.type)
+      ? navRoutes.find(e => e.name === this.props.match.params.type).name
+      : '全部'
+    : navRoutes[0].name
+
+  toggleLoading = (status = false, tip = '再等一下下嘛~') => {
+    this.setState({
+      tip,
+      loading: status
+    })
+  }
+
+  render () {
+    const { children } = this.props
+    const { loading, tip } = this.state
+
+    return (
+      <div className='flex-column' style={{ width: '100%', height: '100%' }}>
+        {/* 顶头菜单,缺省选择项是匹配到nav的路径 */}
+        <Menu
+          style={{ fontSize: 13.5, backgroundColor: '#000' }}
+          mode='horizontal'
+          defaultSelectedKeys={[this.matchRouteName]}
+        >
+          {/* 第一个选项,即首页菜单的链接 */}
+          <Menu.Item
+            style={{
+              marginLeft: 24,
+              marginRight: 30,
+              fontSize: 18,
+              textAlign: 'center',
+              color: '#fff !important',
+              float: 'left'
+            }}
+          >
+            <a href={'/'} className='hover-scale logo-text' style={{
+              color: '#fff2e8'
+            }}>黑骑预告片网站</a>
+          </Menu.Item>
+          {/* 第二个开始的其它选项 */}
+          {
+            navRoutes.map((e, i) => (
+              <Menu.Item key={e.name}>
+                {
+                  getMenuContent({...e})
+                }
+              </Menu.Item>
+            ))
+          }
+        </Menu>
+        <Spin
+          spinning={loading}
+          tip={tip}
+          wrapperClassName='content-spin full'
+        >
+          { children }
+        </Spin>
+      </div>
+    )
+  }
+}
